389718b29ec09304798f281b3d809f5fa270f8ab
Seung Jin <jjiiccsj28@gmail.com>
2024-07-05 16:22:40 +0900

    mysql 연동 login

--- jsp_workspace/jsp_11_database_mysql/mysql.sql
+++ jsp_workspace/jsp_11_database_mysql/mysql.sql
@@ -14,3 +14,69 @@ CREATE TABLE IF NOT EXISTS member(
     name VARCHAR(50),					-- 이름
     addr VARCHAR(300)					-- 주소
 );
+
+-- SELECT * FROM digital_jsp.member;
+SELECT * FROM member;
+
+-- table에 값 삽입, INSERT INTO table VALUES(값...);
+-- num, id, pass, name, addr
+INSERT INTO member VALUES(null,'id001', 'pw001', '김동하','개성');
+
+INSERT INTO member (id,pass,name,addr)
+VALUES('id002','pw002','IRON MAN', 'NEW YORK');
+
+COMMIT;
+
+SELECT * FROM member ORDER BY num DESC;
+
+SELECT num, id, name, addr FROM member ORDER BY num DESC;
+
+DELETE FROM member WHERE id = id001;
+
+DELETE FROM member WHERE  num = 5;
+
+rollback;
+
+SELECT @@autocommit;
+
+SET AUTOCOMMIT = TRUE;
+-- AUTOCOMMIT 해제
+SET AUTOCOMMIT = false;
+
+UPDATE member SET name ='SPIDER MAN', addr = '퀸즈'
+WHERE num='11';
+
+SELECT * FROM member;
+
+SELECT * FROM member WHERE id = 'id001' AND pass='pw001';
+
+commit;
+
+-- SQL Injection
+
+SELECT * FROM member WHERE id = 'id001' ; -- ' AND pass = 'skdjlksdjflksjfdlskjf';
+SELECT * FROM member WHERE id = 'id001' ; -- ' AND pass = 'sfdfs!!!!!!!!!';
+-- ' OR 1 = 1 --
+SELECT * FROM member WHERE id = 'id001' AND pass = '' Or 1 = 1; --
+
+-- PREPARE EXECUTE문
+PREPARE mQuery FROM "SELECT * FROM member WHERE id = ? AND pass =?";
+
+SET @tempVal1 = 'id001';
+SET @tempVal2 = 'pw001';
+
+EXECUTE mQuery USING @tempVal1, @tempVal2;
+
+-- Procedure 
+DELIMITER $$
+	CREATE PROCEDURE loginCheck(
+		IN _id VARCHAR(300),
+        IN _pass VARCHAR(300)
+    )
+BEGIN
+	SELECT * FROM member WHERE id = _id AND pass = _pass;
+END $$
+DELIMITER ;
+
+
+CALL loginCheck('id001', 'pw001');
--- jsp_workspace/jsp_11_database_mysql/src/main/webapp/index.jsp
+++ jsp_workspace/jsp_11_database_mysql/src/main/webapp/index.jsp
@@ -14,8 +14,8 @@
 		// Driver class 이름(위치)
 		String driver = "com.mysql.cj.jdbc.Driver";	
 		// database 에 연결하기 위한 정보
-		String url = "jdbc:mysql://localhost:3306";
-		String username = "root";
+		String url = "jdbc:mysql://localhost:3306/digital_jsp";
+		String username = "digital";
 		String password = "1234";
 		
 		// 연결정보를 이용해서 database에 연결된 정보를 저장하는 객체
@@ -26,11 +26,10 @@
 			out.println("Driver class 존재");
 			
 			conn = DriverManager.getConnection(
-				url, username, password		
+				url,username,password
 			);
 			out.println("DB 연결 완료 : ");
 			out.println(conn + "<br/>");
-			
 		}catch(ClassNotFoundException e){
 			out.println("Driver class를 찾을 수 없음");
 		}catch(SQLException e){
@@ -41,5 +40,15 @@
 			}
 		}
 	%>
+	
+	<h3><a href="insertForm.jsp">회원정보 입력</a></h3>
+	<h3><a href="memberList.jsp">회원 목록</a></h3>
+	<h3><a href="login.jsp">Statement Test</a></h3>
 </body>
 </html>
+
+
+
+
+
+
